c2eade75c391bdeb01b559e21884677421a810a2
Ming Xuan Chai <mx.ch4i@gmali.com>
2017-10-15 13:09:24 +0800

    Halfway commit; store analysis might not be needed?

--- PtrArith.cpp
+++ PtrArith.cpp
@@ -125,6 +125,17 @@ static void escapeAnalysis(const char *name, Module *M)
     V->dump();
 }
 
+int validateName(std::string name) {
+  // Checks if the name is prefixed with %
+  // Used to filter out non-virtual-register names, which can happen in
+  // instructions like `store i32 0, ...`
+  // TODO: this function might be buggy since I'm trying to filter out names
+  // that don't have %, and for now the only ones that don't seem to have % in
+  // them are '0', so name[0] won't even work because it's like a null string?
+  return name.length() > 0;
+  //return name[0] == '%';
+}
+
 /*
  * First Pass of the algorithm
  * Runs through the LLVM instructions and maps all seen Virtual
@@ -147,7 +158,10 @@ static void mapRegsToType(const char *name, Module *M) {
     // Might include values we don't need, since LLVM auto-generated functions
     // would be here too, not just the source code declared functions
     for (auto &A : F.getArgumentList()) {
-      name_type_map.insert(make_pair(A.getName().str(), A.getType()));
+      std::string arg_name = A.getName().str();
+      //errs() << "arg name: " << arg_name << "\n";
+      if (validateName(arg_name))
+        name_type_map.insert(make_pair(A.getName().str(), A.getType()));
     }
 
     for (auto &BB : F) {
@@ -156,52 +170,89 @@ static void mapRegsToType(const char *name, Module *M) {
         Value *val_ptr = nullptr; // not sure what this is needed for atm
         PointerType *ptr_type = nullptr;
         std::string name = "";
-  
-        // Map name to type for alloca instructions
+ 
+        if (isa<llvm::BinaryOperator>(&I)) {
+          errs() << "==================" << "\n";
+          I.dump();
+          errs() << "==================" << "\n";
+          errs() << "OMG I FOUND A BINARRYYY OPERAAAATORRRR" << "\n";
+        }
+
+
+        // Check if I is AllocaInst
         if (AllocaInst *AI = dyn_cast<AllocaInst>(&I)) {
           // getType() returns pointer to PointerType
           ptr_type = AI->getType();
           name = AI->getName().str();
 
-          // Print out nicely, can remove in the future
-          //errs() << name << " has type: ";
-          //ptr_type->dump(); 
-          //errs() << "\n";
+          errs() << "==================" << "\n";
+          I.dump();
+          errs() << "==================" << "\n";
+          errs() << name << " has type: ";
+          ptr_type->dump(); 
+          errs() << "\n";
 
           // Insert into map
           name_type_map.insert(make_pair(name, ptr_type));
         }
 
+        // Check if I is GetElementPtrInst
+        if (GetElementPtrInst *GEPI = dyn_cast<GetElementPtrInst>(&I)) {
+          errs() << "==================" << "\n";
+          I.dump();
+          errs() << "==================" << "\n";
+          Type *ptr_operand_type = GEPI->getPointerOperandType();
+          errs() << "Pointer Operand Type of GEP: ";
+          ptr_operand_type->dump();
+          errs() << "\n";
+          name = GEPI->getName().str();
+          errs() << "Name of lvalue: " << name << "\n";
+          name_type_map.insert(make_pair(name, ptr_operand_type));
+        }
+
         // Check if I is load
-        if (LoadInst *ret = dyn_cast<LoadInst>(&I)) {
-          // do stuff
+        if (LoadInst *LI = dyn_cast<LoadInst>(&I)) {
+          errs() << "==================" << "\n";
+          I.dump();
+          errs() << "==================" << "\n";
+          // Extract the operands 
+          val_ptr = LI->getPointerOperand();
+          errs() << "Pointer Operand: ";
+          val_ptr->dump();
+          errs() << "xxxxxxxxxxxxxxxx" << "\n";
+
+          Type *ptr_operand_type = LI->getPointerOperand()->getType();
+          errs() << "Pointer Operand Type of LI: ";
+          ptr_operand_type->dump();
+          errs() << "\n";
+
+          // Load instructions apparently don't have explicit names...
+          if ((name = LI->getName()) != NULL) {
+            // Manually extract the name
+          }
+          //name_type_map.insert(make_pair(name, ptr_operand_type));
         }
 
         // Check if I is store
-        if (StoreInst *ret = dyn_cast<StoreInst>(&I)) {
-          // do stuff
-        }
+        //if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
+        //  errs() << "==================" << "\n";
+        //  I.dump();
+        //  errs() << "==================" << "\n";
+        //  // Extract the operands 
+        //  Value *val_operand = SI->getValueOperand();
+        //  Value *ptr_operand = SI->getPointerOperand();
+        //  errs() << "Value Operand of Store: " << val_operand->getName().str() << "\n";
+        //  errs() << "Pointer Operand of Store: " << ptr_operand->getName().str() << "\n";
 
-        // Check if I is store
-        if (GetElementPtrInst *ret = dyn_cast<GetElementPtrInst>(&I)) {
-          // do stuff
-        }
+        //  // Add PointerOperand to name_type_map with type of ValueOperand
+        //  std::string val_operand_name = val_operand->getName().str();
+        //  if (validateName(val_operand_name)) {
+        //    llvm::Type *val_operand_type = name_type_map.at(val_operand->getName().str());
+        //    std::string ptr_operand_name = ptr_operand->getName().str();
+        //    name_type_map.insert(make_pair(ptr_operand_name, val_operand_type));
+        //  }
 
-        // Check if is an escaping instruction:
-        Value *Ptr = nullptr;       // Escaping pointer
-        if (ReturnInst *Ret = dyn_cast<ReturnInst>(&I))
-          Ptr = Ret->getReturnValue();
-        else if (StoreInst *Store = dyn_cast<StoreInst>(&I))
-        {
-          // For the bonus:
-          Ptr = Store->getPointerOperand();
-          if (!isPointerInteresting(Ptr, interesting))
-            continue;
-          Ptr = Store->getValueOperand();
-        }
-        if (Ptr == nullptr || !isa<PointerType>(Ptr->getType()))
-          continue;
-        addEscapingAllocas(Ptr, seen, escaping);
+        //}
       }
     }
   }
