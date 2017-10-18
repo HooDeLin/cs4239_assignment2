be9d6bcde387279b085268b50e71f78d4ff42818
HooDeLin <hoodelin0619@gmail.com>
2017-10-19 04:24:17 +0800

    Refactored code

--- PtrArith.cpp
+++ PtrArith.cpp
@@ -8,7 +8,9 @@
 
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/Constants.h"
-
+#include "llvm/ADT/Twine.h"
+// #include "llvm/IR/DerivedUser.h"
+#include "llvm/IR/Operator.h"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -45,7 +47,7 @@ std::string trimWhitespace(std::string input) {
   return output;
 }
 
-std::string getOperandFromLoad(std::string instruction) {
+std::string getOperandFromInstStringManually(std::string instruction) {
   std::stringstream ss(instruction);
   std::string item;
   vector<std::string> tokens;
@@ -55,21 +57,24 @@ std::string getOperandFromLoad(std::string instruction) {
   return trimWhitespace(tokens.at(0)).erase(0,1);
 }
 
+/* Functions to get std::string representation from LLVM classes */
+
 std::string getStringFromValuePtr(Value * val) {
   // Returns the virtual register name from a Value *
+  if (val->hasName()) {
+    return val->getName().str();
+  }
   std::string instruction, val_str;
   raw_string_ostream rso(instruction);
   val->print(rso);
-  val_str = getOperandFromLoad(instruction);
-  return val_str;
+  return getOperandFromInstStringManually(instruction);
 }
 
 std::string getStringFromTypePtr(Type *type) {
   std::string type_string;
   raw_string_ostream rso(type_string);
   type->print(rso);
-  type_string = rso.str();
-  return type_string;
+  return rso.str();
 }
 
 std::string getStringFromInstPtr(Instruction *inst) {
@@ -79,43 +84,82 @@ std::string getStringFromInstPtr(Instruction *inst) {
   return instruction;
 }
 
-std::string getPointerOperandFromLoadInst(LoadInst *loadInst) {
-  if (loadInst->getPointerOperand()->getName().str().compare("")) {
-    return loadInst->getPointerOperand()->getName().str();
+/* End of Functions to get std::string representation from LLVM classes */
+
+
+/* Functions to get std::string operands from Instructions */
+
+std::string getOperandFromInst(Instruction *instruction) {
+  if (instruction->hasName()) {
+    return instruction->getName().str();
+  }
+  std::string instruction_string = getStringFromInstPtr(instruction);
+  return getOperandFromInstStringManually(instruction_string);
+}
+
+std::string getPointerOperandFromInst(Instruction *instruction, Value *pointerOperand) {
+  if (pointerOperand->hasName()) {
+    return pointerOperand->getName().str();
   } else {
-    std::string type = getStringFromTypePtr(loadInst->getPointerOperand()->getType());
-    std::string instruction = getStringFromInstPtr(loadInst);
-    size_t pos = instruction.find(type);
-    instruction.erase(0, pos + type.length());
-    pos = instruction.find(',');
-    std::string pointerOperand = trimWhitespace(instruction.substr(0, pos));
+    std::string type = getStringFromTypePtr(pointerOperand->getType());
+    std::string instruction_string = getStringFromInstPtr(instruction);
+    size_t pos = instruction_string.find(type);
+    instruction_string.erase(0, pos + type.length());
+    pos = instruction_string.find(',');
+    std::string pointerOperand = trimWhitespace(instruction_string.substr(0, pos));
     return pointerOperand.erase(0,1);
   }
 }
 
-std::string getPointerOperandFromGepInst(GetElementPtrInst *GEPI) {
-  Type *ptr_operand_type = GEPI->getPointerOperandType();
-  std::string type = getStringFromTypePtr(ptr_operand_type);
-  std::string instruction = getStringFromInstPtr(GEPI);
-  size_t pos = instruction.find(type);
-  instruction.erase(0, pos + type.length());
-  pos = instruction.find(',');
-  std::string pointerOperand = trimWhitespace(instruction.substr(0, pos));
-  return pointerOperand.erase(0,1);
+/* End of Functions to get std::string operands from Instructions */
+
+/* Helper functions */
+bool isDoingPtrArith(GetElementPtrInst *GEPI) {
+  // We can know if GetElementPtrInst is doing pointer arithmetic if some indices are non zero
+  // and the register value looks like %incdec.ptr, %incdec.ptr1, %incdec.ptr2 ...
+  return !GEPI->hasAllZeroIndices() && !getOperandFromInst(GEPI).substr(0,10).compare("incdec.ptr");
 }
 
-/*
- * First Pass of the algorithm
- * Runs through the LLVM instructions and maps all seen Virtual
- * Registers to their LLVM types
- */
-static void mapRegsToType(const char *name, Module *M) {
+bool isValueNameEmpty(Value * val) { // A value can be %a, %1 or 0
+  if (val->hasName()) { // No need for more checks
+    return true;
+  }
+  std::string instruction, val_str;
+  raw_string_ostream rso(instruction);
+  val->print(rso);
+  std::stringstream ss(instruction);
+  std::string item;
+  vector<std::string> tokens;
+  while(std::getline(ss, item, '=')) {
+    tokens.push_back(item);
+  }
+  return trimWhitespace(tokens.at(0)).at(0) == '%';
+}
+/* End of Helper functions */
+
+/* Function to print warnings */
+
+void printDetectedAnalysis(GetElementPtrInst *GEPI, std::string functionName) {
+  if (MDNode *n = GEPI->getMetadata("dbg")) {
+    DILocation loc(n);
+    unsigned line = loc.getLineNumber();
+    StringRef file = loc.getFilename();
+    StringRef dir = loc.getDirectory();
+    errs() << "Line " << line << " of " << dir.str() << "/"
+           << file.str() << ": Possible pointer arithmetic on non-array objects\n";
+  } else {
+    errs() << "Write into string literal in function "
+           << functionName << "\n";
+  }
+}
+
+/* End of Function to print warnings */
+
+/* Main Analyse and Instruction Loop */
+static void analyse(const char *name, Module *M) {
   map<std::string, Type *> name_type_map;
   map<std::string, std::string> reg_relation_map;
 
-  // `auto` keyword in C++11 means automatic type inference
-  // Prior to C++11, it meant automatic lifetime, which was implicitly declared
-  // C++11 Ranged-based for loop
   for (auto &F : *M) {
     // To catch types of parameters
     // Might include values we don't need, since LLVM auto-generated functions
@@ -130,29 +174,26 @@ static void mapRegsToType(const char *name, Module *M) {
 
     for (auto &BB : F) {
       for (auto &I : BB) {
+        // errs() << "==================" << "\n";
+        // I.dump();
+        // errs() << "\n";
+        // errs() << "==================" << "\n";
         // Variables
         Value *val_ptr = nullptr;
         PointerType *ptr_type = nullptr;
         std::string name = "";
 
         if (isa<llvm::BinaryOperator>(&I)) {
-          // errs() << "==================" << "\n";
-          // I.dump();
-          // errs() << "==================" << "\n";
           Value *op1 = I.getOperand(0);
           Value *op2 = I.getOperand(1);
           Type *op1_type = op1->getType();
 
           // Get the string representation of the virtual register
-          std::string op1_str, op2_str;
-          op1_str = getStringFromValuePtr(op1);
-          op2_str = getStringFromValuePtr(op2);
+          std::string op1_str = getStringFromValuePtr(op1);
+          std::string op2_str = getStringFromValuePtr(op2);
 
           // Extract the lvalue's name
-          std::string instruction, name;
-          raw_string_ostream rso(instruction);
-          I.print(rso);
-          name = getOperandFromLoad(instruction);
+          name = getOperandFromInst(&I);
 
           // Type of both operands and result will be that of op1
           name_type_map.insert(make_pair(op1_str, op1_type));
@@ -167,38 +208,29 @@ static void mapRegsToType(const char *name, Module *M) {
           // errs() << "\n";
         }
 
-
-        // Check if I is AllocaInst
         if (AllocaInst *AI = dyn_cast<AllocaInst>(&I)) {
-          // getType() returns pointer to PointerType
           ptr_type = AI->getType();
           name = AI->getName().str();
-
-          // errs() << "==================" << "\n";
-          // I.dump();
-          // errs() << "==================" << "\n";
           // errs() << name << " has type: ";
           // ptr_type->dump();
           // errs() << "\n";
-          // Insert into map
           name_type_map.insert(make_pair(name, ptr_type));
         }
 
-        // Check if I is GetElementPtrInst
         if (GetElementPtrInst *GEPI = dyn_cast<GetElementPtrInst>(&I)) {
-          // errs() << "==================" << "\n";
-          // I.dump();
-          // errs() << "==================" << "\n";
           Type *ptr_operand_type = GEPI->getPointerOperandType();
           // errs() << "Pointer Operand Type of GEP: ";
           // ptr_operand_type->dump();
           // errs() << "\n";
-          name = GEPI->getName().str();
+          name = getOperandFromInst(GEPI);
           // errs() << "Name of lvalue: " << name << "\n";
-          std::string ptr_operand = getPointerOperandFromGepInst(GEPI);
+          std::string ptr_operand = getPointerOperandFromInst(GEPI, GEPI->getPointerOperand());
           name_type_map.insert(make_pair(name, ptr_operand_type));
-          if (!GEPI->hasAllZeroIndices() && !name.substr(0,10).compare("incdec.ptr")) {
-            // Check if this is from an array (ptr_operand)
+          reg_relation_map.insert(make_pair(name, ptr_operand));
+
+          // Analyse and report non-array pointer arithmetic
+          if (isDoingPtrArith(GEPI)) {
+            // Backtrack at most 2 levels to check if it is an array
             std::string current = ptr_operand;
             if (reg_relation_map.find(current) != reg_relation_map.end()) {
               current = reg_relation_map.at(current);
@@ -207,85 +239,50 @@ static void mapRegsToType(const char *name, Module *M) {
               }
             }
             if (!name_type_map.at(current)->getArrayElementType()->isArrayTy()) {
-              if (MDNode *n = GEPI->getMetadata("dbg")) {
-                DILocation loc(n);
-                unsigned line = loc.getLineNumber();
-                StringRef file = loc.getFilename();
-                StringRef dir = loc.getDirectory();
-                errs() << "Line " << line << " of " << dir.str() << "/"
-                       << file.str() << ": Possible pointer arithmetic on non-array objects\n";
-              } else {
-                errs() << "Write into string literal in function "
-                       << F.getName().str() << "\n";
-              }
+              printDetectedAnalysis(GEPI, F.getName().str());
             }
           }
-          reg_relation_map.insert(make_pair(name, ptr_operand));
         }
 
         // Check if I is load
         if (LoadInst *LI = dyn_cast<LoadInst>(&I)) {
-          // errs() << "==================" << "\n";
-          // I.dump();
-          // errs() << "==================" << "\n";
-          // Extract the operands
-          val_ptr = LI->getPointerOperand();
-          // errs() << "Pointer Operand: ";
-          // val_ptr->dump();
-
           Type *ptr_operand_type = LI->getPointerOperand()->getType();
           // errs() << "Pointer Operand Type of LI: ";
           // ptr_operand_type->dump();
           // errs() << "\n";
 
           // Extract the lvalue's name
-          std::string instruction;
-          raw_string_ostream rso(instruction);
-          I.print(rso);
-          name = getOperandFromLoad(instruction);
-          std::string pointerOperand = getPointerOperandFromLoadInst(LI);
+          name = getOperandFromInst(LI);
+          std::string pointerOperand = getPointerOperandFromInst(LI, LI->getPointerOperand());
           reg_relation_map.insert(make_pair(name, pointerOperand));
           name_type_map.insert(make_pair(name, ptr_operand_type));
         }
 
         // Check if I is store
         if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
-          // errs() << "==================" << "\n";
-          // I.dump();
-          // errs() << "\n";
-          // errs() << "==================" << "\n";
           // Extract the operands
           Value *val_operand = SI->getValueOperand();
           Value *ptr_operand = SI->getPointerOperand();
+          std::string val_operand_string = getStringFromValuePtr(val_operand);
+          std::string ptr_operand_string = getStringFromValuePtr(ptr_operand);
           // errs() << "Value Operand of Store: " << val_operand->getName().str() << "\n";
           // errs() << "Pointer Operand of Store: " << ptr_operand->getName().str() << "\n";
 
-          // Add PointerOperand to name_type_map with type of ValueOperand
-          if (ConstantExpr *expr = dyn_cast<ConstantExpr>(val_operand)) { //handle inner instructions
+          // This happens whenever it is a global array or global structs
+          // Example: store i32* getelementptr inbounds ([4 x i32]* @a, i32 0, i32 0), i32** %b, align 8
+          // Example: store i32* getelementptr inbounds (%struct.numbers* @mynumber, i32 0, i32 0), i32** %b, align 8
+          if (ConstantExpr *expr = dyn_cast<ConstantExpr>(val_operand)) {
             if (GetElementPtrInst *GEPI = dyn_cast<GetElementPtrInst>(expr->getAsInstruction())) {
                 if (GEPI->getPointerOperandType()->getArrayElementType()->isArrayTy()) {
-                  name_type_map.erase(ptr_operand->getName().str());
-                  name_type_map.insert(make_pair(ptr_operand->getName().str(), GEPI->getPointerOperandType()));
+                  // We need to update the newest type
+                  name_type_map.erase(ptr_operand_string);
+                  name_type_map.insert(make_pair(ptr_operand_string, GEPI->getPointerOperandType()));
                 }
             }
-          } else {
-            if (val_operand->getName().str().compare("")) {
-              reg_relation_map.insert(make_pair(ptr_operand->getName().str(), val_operand->getName().str()));
-              if (name_type_map.find(val_operand->getName().str()) == name_type_map.end()) {
-                name_type_map.insert(make_pair(val_operand->getName().str(), val_operand->getType()));
-              }
-            }
-            std::string val_operand_name = val_operand->getName().str();
-            if (validateName(val_operand_name)) {
-              auto test = name_type_map.find(val_operand->getName().str());
-              if (test != name_type_map.end()) {
-                llvm::Type *val_operand_type = name_type_map.at(val_operand->getName().str());
-                std::string ptr_operand_name = ptr_operand->getName().str();
-                name_type_map.insert(make_pair(ptr_operand_name, val_operand_type));
-              } else {
-                // errs() << val_operand_name << " is not in the map" << "\n";
-              }
-            }
+          } else if (isValueNameEmpty(val_operand)) {
+            reg_relation_map.insert(make_pair(ptr_operand_string, val_operand_string));
+            name_type_map.insert(make_pair(val_operand_string, val_operand->getType()));
+            name_type_map.insert(make_pair(ptr_operand_string, val_operand->getType()));
           }
         }
       }
@@ -323,7 +320,7 @@ int main(int argc, char **argv)
       continue;
     }
     // our analysis function
-    mapRegsToType(argv[i], M);
+    analyse(argv[i], M);
   }
   return 0;
 }
